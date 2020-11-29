38411d9ec7d5a015dc26b86a5c4e4a6dae78b657
gauriamane <61306300+gauriamane@users.noreply.github.com>
2020-11-29 12:57:07 +0530

    Final commit

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {FaHome} from 'react-icons/fa';
+import { FaCalendar } from 'react-icons/fa'
+import {FaTable} from 'react-icons/fa';
+
+export const Nav = () => (
+    <nav className="nav">
+        {/* <Link to="/">
+            <FaHome />
+        </Link> */}
+      
+    </nav>
+)
